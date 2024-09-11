53e4475201f19827ffff3e6c9c5c68e2c7db334a
ANGELICA ALONSO ORTIZ <130294701+ANGELICA-DATA-SCIENCE@users.noreply.github.com>
2024-09-11 15:58:30 -0600

    Se creo el primer proyecto de ciclos anidados y se modificó el .gitignore

---
+++ src/TrianguloNormal.java
@@ -0,0 +1,15 @@
+
+public class TrianguloNormal {
+
+	public static void main(String[] args) {
+		int star=6;
+		for (int i=1; i<=star; i++) {   //inicializador; condicion; incremental;
+			for(int star2 = 0; star2<i; star2++) {
+				System.out.print("*");//imprimir sin salto de linea
+			}//for2
+			System.out.println(); //aqui imprime el salto de linea despues de los asteriscos o estrellas
+
+	}//for1
+
+}//main
+}//class TrianguloNormal
